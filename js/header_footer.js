33972e3e952a33e0ed08e3ae141185bb35a382fb
nyxxxxx <s0941212@gmail.com>
2023-05-21 20:48:41 +0800

    主題色+footer完成 header、footer改由js檔載入

---
+++ js/header_footer.js
@@ -0,0 +1,85 @@
+
+$('header').append(`        <!-- navbar -->
+    <div class="navbar-div row mx-auto flex-column">
+
+        <div class="searchbar col-12 d-flex justify-content-center">
+            <input type="text" placeholder="搜尋想要的創作">
+            <button type="submit">Search</button>
+        </div>
+
+        <div class="nav-content col-xl-6 col-lg-8 col-md-10 mx-auto">
+
+            <ul class="d-flex justify-content-center align-items-center list-unstyled">
+                <h3 class="web-title text-center"> 民藝集市</h3>
+                <li class="drop-item mx-auto"><a href="#">找創作</a></li>
+                <li class="drop-item mx-auto">
+                    <a href="#">優惠專區</a>
+                    <ul class="d-menu list-unstyled">
+                        <li><a href="#">優惠推銷</a></li>
+                        <li><a href="#">母親節特賣</a></li>
+                        <li><a href="#">免運專區</a></li>
+                    </ul>
+                </li>
+
+                <li class="drop-item mx-auto">
+                    <a href="#">商品誌</a>
+                    <ul class="d-menu list-unstyled">
+                        <li><a href="#">商品誌</a></li>
+                        <li><a href="#">人物專訪</a></li>
+                        <li><a href="#">商品理念</a></li>
+                    </ul>
+                </li>
+
+                <li class="drop-item mx-auto">
+                    <a href="#">會員中心</a>
+                    <ul class="d-menu list-unstyled">
+                        <li><a href="#">查詢訂單</a></li>
+                        <li><a href="#">會員中心</a></li>
+                    </ul>
+                </li>
+
+                <li class="drop-item mx-auto">
+                    <a href="#">關於我們</a>
+                    <ul class="d-menu list-unstyled">
+                        <li><a href="#">網站理念</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
+    </div>`);
+
+$('footer').append(`
+    <div class="footer-container d-flex justify-content-center">
+    <div class="footer-aside col-2">
+        <h4>Ming-Yi</h4>
+        <ul class="footer-list list-unstyled col-xl-6 ">
+            <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;關於民藝</a></li>
+            <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;聯絡我們</a></li>
+            <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;人才招募</a></li>
+            <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;尋找創作</a></li>
+            <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;販售設計</a></li>
+        </ul>
+    </div>
+    <div class="footer-list footer-aside col-2">
+        <h4> Category</h4>
+        <ul class="list-unstyled col-xl-6">
+                <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;飾品</a></li>
+                <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;衣裝</a></li>
+                <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;生活雜貨</a></li>
+                <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;藝術作品</a></li>
+                <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;辦公用品</a></li>                        
+        </ul>
+    </div>
+
+    <div class="footer-list footer-aside col-2">
+        <h4> Contact us</h4>
+        <ul class="list-unstyled col-xl-6">
+                <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;推特</a></li>
+                <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;IG</a></li>
+                <li><a href="#" class="text-decoration-none">&nbsp;&nbsp;&nbsp;FB</a></li>                       
+        </ul>
+    </div>
+    </div>
+    <div class="footer-sub text-center"><h6>© 2023 Ming-Yi, Inc. | All rights reserved. </h6></div>
+    `);
